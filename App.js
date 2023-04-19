a3dedab6ce6b39bdaea5e15ab2287d6ec8d6618c
[rawan fouda] <[rawanfouda840@gmail.com]>
2023-04-19 22:25:05 +0200

    remove react voice/voice and add translation files

--- App.js
+++ App.js
@@ -1,38 +1,70 @@
-   import React from 'react';
-   import { NavigationContainer } from '@react-navigation/native';
-   import { createNativeStackNavigator } from '@react-navigation/native-stack';
-   import Welcomepage from './pages/welcomePage/Welcomepage';
-  import Splashscreen from './pages/splashScreen/Splashscreen';
-  import Signiturepage from './pages/signiturePage/Signiturepage';
-  import Camerapage from './pages/cameraPage/Camerapage';
-   const App = () => {
-    const Stack = createNativeStackNavigator();
-     return (
-      <>
-       <NavigationContainer >
-       <Stack.Navigator>
-        <Stack.Screen  options={{headerShown: false}}name="Splashscreen" component={Splashscreen} />
-        <Stack.Screen  options={{headerShown: false}}name="Welcomepage" component={Welcomepage} />
-        <Stack.Screen  options={{headerShown: false}}name="Signiturepage" component={Signiturepage}/>  
-        <Stack.Screen  options={{headerShown: false}}name="Camerapage" component={Camerapage} />  
-      </Stack.Navigator>
-        </NavigationContainer>
-      </>
-     );
-   };
-  export default App;
+  //  import React from 'react';
+  //  import { NavigationContainer } from '@react-navigation/native';
+  //  import { createNativeStackNavigator } from '@react-navigation/native-stack';
+  //  import Welcomepage from './pages/welcomePage/Welcomepage';
+  // import Splashscreen from './pages/splashScreen/Splashscreen';
+  // import Signiturepage from './pages/signiturePage/Signiturepage';
+  // import Camerapage from './pages/cameraPage/Camerapage';
+  //  const App = () => {
+  //   const Stack = createNativeStackNavigator();
+  //    return (
+  //     <>
+  //      <NavigationContainer >
+  //      <Stack.Navigator>
+  //       <Stack.Screen  options={{headerShown: false}}name="Splashscreen" component={Splashscreen} />
+  //       <Stack.Screen  options={{headerShown: false}}name="Welcomepage" component={Welcomepage} />
+  //       <Stack.Screen  options={{headerShown: false}}name="Signiturepage" component={Signiturepage}/>  
+  //       <Stack.Screen  options={{headerShown: false}}name="Camerapage" component={Camerapage} />  
+  //     </Stack.Navigator>
+  //       </NavigationContainer>
+  //     </>
+  //    );
+  //  };
+  // export default App;
 
+ 
 
-
+import React ,{ useState, useEffect} from 'react';
+  import DropDownPicker from 'react-native-dropdown-picker';
+  import { Text , View } from 'react-native';
+  import { useTranslation ,changeLanguage } from 'react-i18next';
+  import i18n from 'i18next';
+  function App() {
+    const {t, i18n} =useTranslation(); // destructure i18n here
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState('en');
+    const [items, setItems] = useState([
+      {label: 'English', value: 'en'},
+      {label: 'Arabic', value: 'ar'},
+    ]);
+  
+    useEffect(() => {
+      i18n.changeLanguage(value);
+    }, [value]);
+  
+    return (
+      <View>
+        <Text>{t('start now')}</Text>
+        <DropDownPicker
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+        />
+      </View>
+    );
+  }
+export default App;
 
 
   // import { StatusBar } from 'expo-status-bar';
   // import { StyleSheet, Text, Button, View } from 'react-native';
   // import { en, ar } from './localizations';
   // import { useState } from 'react';
-  // import * as Localization from 'expo-localization';
   // import {I18n} from 'i18n-js';
-  
+  // import * as Localization from 'expo-localization';
   // export default function App() {
   //   let [locale, setLocale] = useState(Localization.locale);
   //   I18n.fallbacks = true;
---
+++ I18n/en.js
@@ -0,0 +1,8 @@
+export default{
+    about:"EarHear uses AI to translate sign language and make the communication with each other get easier.",
+    aboutApp:"About App",
+    startNow:"Start Now",
+    typeToTranslate:"Type to translate",
+    camera:"Camera",
+    voice:"Voice"
+}
